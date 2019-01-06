de182574c2d5b801eff3c10c2a24bad55822b8c8
Manishankar Vakta <evilrider555@gmail.com>
2019-01-06 12:12:44 +0600

    Initial commit

---
+++ app/src/main/java/com/timebdshop/timebdshop/MainActivity.java
@@ -0,0 +1,65 @@
+package com.timebdshop.timebdshop;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        boolean connected = false;
+//        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+//        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+//                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+//            //we are connected to a network
+//            connected = true;
+//
+//        }
+//        else{
+//            connected = false;
+//        }
+
+
+        //Create Web View
+        WebView webb = (WebView)findViewById(R.id.webview1);
+        webb.getSettings().setJavaScriptEnabled(true);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("http://timebdshop.com");
+
+        //Hook back key with application
+        webb.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+    }
+}
